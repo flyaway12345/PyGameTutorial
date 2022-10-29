ea540651f0a7aa2c22353e605ffb7226a5ba06c9
flyaway12345 <evanhaggard123@gmail.com>
2022-10-29 13:55:37 -0500

    ye

--- runner_class only.py
+++ runner_class only.py
@@ -1,3 +1,4 @@
+from itertools import count
 import pygame
 from sys import exit 
 
@@ -11,8 +12,10 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
+
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-text_surf = test_font.render('Bongwater',False,'Black')
+text_surf = test_font.render("Bongwater",False,'Black')
+text_rect = text_surf.get_rect(center = (400,100))
 
 snail_surf = pygame.image.load('graphics\snail\snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom =(700,300))
@@ -26,14 +29,17 @@ while True:
             pygame.quit()
             exit()
 
+
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surf,(0,0))
+    screen.blit(text_surf,text_rect)
     
 
     if snail_rect.right <=0: snail_rect.left = 800
     snail_rect.x -= 4
     screen.blit(snail_surf,snail_rect)
     screen.blit(player_surf,player_rect)
+
+
     pygame.display.update()
     clock.tick(60)
